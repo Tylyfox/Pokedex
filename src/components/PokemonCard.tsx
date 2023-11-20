0f78dd90d27bca217847cf2e5e3b039e2f8f9b95
cadart <cadart.valentin@sfr.fr>
2023-11-20 21:48:25 +0100

    add dynamique pokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,32 @@
+import "../App.css";
+
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+        name: "mew",
+    }
+]
 
 function PokemonCard () {
+    const pokemon = pokemonList[0];
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon image" />
+        <figure className="card">
+            {
+                pokemon.imgSrc ? 
+                <img 
+                    src={pokemon.imgSrc} 
+                    alt="pokemon image"
+                    className="card-img" 
+                    /> 
+                    : 
+                    <p>???</p>
+            }
             <figcaption>
-                <p>Bulbausur</p>
+                {pokemon.name}
             </figcaption>
         </figure>
     )
